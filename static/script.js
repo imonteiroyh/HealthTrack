3cebd9986d1694cc388a7e08047e1a1d084a1c90
imonteiroyh <imonteiroyh@gmail.com>
2023-04-20 07:05:57 -0300

    Adicionado arquivo HTML para o perfil 2 - Classificação de Risco

--- static/script.js
+++ static/script.js
@@ -1,15 +1,3 @@
-// // Get the dropdown button
-// var dropdownBtn = document.getElementsByClassName("dropbtn")[0];
-
-// // When the user clicks on the button, toggle between showing and hiding the dropdown content
-// dropdownBtn.addEventListener("click", function() {
-//   var dropdownContent = this.nextElementSibling;
-//   if (dropdownContent.style.display === "block") {
-//     dropdownContent.style.display = "none";
-//   } else {
-//     dropdownContent.style.display = "block";
-//   }
-// });
 document.addEventListener('DOMContentLoaded', function() {
     var dropdownBtn = document.getElementsByClassName("dropbtn")[0];
     dropdownBtn.addEventListener("click", function() {
