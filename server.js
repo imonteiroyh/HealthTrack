b2b864ce669616e877f68aeb2bba6afd653ed2b2
imonteiroyh <imonteiroyh@gmail.com>
2023-04-22 08:02:52 -0300

    Adicionando perfis de atendimento e classificação de risco

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-const http = require("http");
-const fs = require("fs").promises;
+import express from 'express';
 
+const app = express();
+const port = 8080;
 
-const host = "localhost";
-const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'))
 
-const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/index.html")
-        .then(contents => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        });
-};
+var initials = 'VM';
 
-const server = http.createServer(requestListener)
-    server.listen(port, host, () => {
+const profiles = [
+    'attendant'
+]
+
+app.get('/', (req, res) => {
+    res.render('consultation-queue', {
+        initials: initials,
+        profile: profiles[0]
+    });
+});
+
+// Attendant routes
+app.get('/register-patient', (req, res) => {
+    res.render('register-patient', {
+       initials: initials
+    });
+});
+
+app.get('/register-consultation', (req, res) => {
+    res.render('register-consultation', {
+       initials: initials
+    });
+});
+
+// Risk classification routes
+app.get('/consultation-queue', (req, res) => {
+    res.render('consultation-queue', {
+        initials: initials
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
