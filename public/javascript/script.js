a1c7e19f765bf626bf4ea6c55ab07203d55a3665
imonteiroyh <imonteiroyh@gmail.com>
2023-04-22 08:01:35 -0300

    Adicionando código javascript, arquivos css e imagens utilizadas

---
+++ public/javascript/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownBtn = document.getElementsByClassName("dropbtn")[0];
+    dropdownBtn.addEventListener("click", function() {
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display == "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    })
+})
